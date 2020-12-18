947de9976f0fd63f92a85532b29c9006f511ea1b
Richard Morrisey <r.morrisey@computer.org>
2020-12-18 14:43:23 -0500

    modes and submodes with test

--- type_definitions_test.go
+++ type_definitions_test.go
@@ -3,9 +3,38 @@ package main
 import "testing"
 
 func TestModes(t *testing.T) {
-	var m Mode = AM
-	if m.String() != "AM" {
-		t.Error("AM.String() should equal \"AM\"")
+	for m := AM_MODE; m <= THRBX_MODE; m++ {
+		var i interface{} = m.String()
+		r, ok := i.(string)
+		if !ok {
+			t.Errorf("ERROR: Expected type string got %v", r)
+		}
+	}
+}
+
+func TestImportOnly(t *testing.T) {
+
+	for m := AM_MODE; m <= WSPR_MODE; m++ {
+		if m.isImportOnly() {
+			t.Errorf("ERROR: Did not expect %v to be Import Only", m)
+		}
 	}
 
+	for m := AMTORFEC_MODE; m <= THRBX_MODE; m++ {
+		if !m.isImportOnly() {
+			t.Errorf("ERROR: Expected %v to be Import Only", m)
+		}
+	}
+
+}
+
+func TestSubmodes(t *testing.T) {
+
+	for s := ASCI_SUBMODE; s <= USB_SUBMODE; s++ {
+		var i interface{} = s.String()
+		r, ok := i.(string)
+		if !ok {
+			t.Errorf("ERROR: Expected type string got %v", r)
+		}
+	}
 }
